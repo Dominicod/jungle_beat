10f6e3e66cf2c10726a197ece3f529e6ac71c8ff
dominicod <dominicodonnell99@gmail.com>
2022-10-03 17:22:13 -0500

    Test: create tests for multiple node structure

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -23,22 +23,34 @@ RSpec.describe LinkedList do
       expect(list.head).to be_an_instance_of(Node)
       expect(list.head.next_node).to eq nil
       expect(list.head.data).to eq 'doop'
+
+      list.append('deep')
+      next_node_data = list.head.next_node.data
+      next_node_nn = list.head.next_node.next_node
+
+      expect(list.head.next_node).to be_an_instance_of(Node)
+      expect(next_node_nn).to eq nil
+      expect(next_node_data).to eq 'deep'
     end
   end
 
   describe '.count' do
     it 'returns the count of things in the list' do
       list.append('doop')
-
       expect(list.count).to eq 1
+
+      list.append('deep')
+      expect(list.count).to eq 2
     end
   end
 
   describe '.to_string' do
     it 'generates a string of all the elements in the list, separated by spaces' do
       list.append('doop')
-
       expect(list.to_string).to eq 'doop'
+
+      list.append('deep')
+      expect(list.to_string).to eq 'doop deep'
     end
   end
 end
