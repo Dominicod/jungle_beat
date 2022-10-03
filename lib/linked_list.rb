648f1e78a4b7789d8b5f3bc21d1f32678b781b8f
dominicod <dominicodonnell99@gmail.com>
2022-10-03 17:05:10 -0500

    Feat: create helper methods for LinkedList class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,7 +2,21 @@
 
 # creates the LinkedList of nodes
 class LinkedList
+  attr_reader :head
+
   def initialize
     @head = nil
+  end
+
+  def append(data)
+    @head = Node.new(data)
+  end
+
+  def count
+    @head.count
+  end
+
+  def to_string
+    @head.data
   end
 end
